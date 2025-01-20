3f61f032be1814c6f3918b5a4f2911c044d4c317
rohanbagel <gitgud1211@gmail.com>
2025-01-20 10:35:57 +0530

    worked on scraper.py(not running well)

--- Selena/scraper.py
+++ Selena/scraper.py
@@ -1,28 +1,126 @@
 import logging, os
+import time
 import json, csv
 from dataclasses import dataclass,field,fields, asdict
 import requests
-from bs4 import BeautifulSoup   
+from bs4 import BeautifulSoup 
+from concurrent.futures import ThreadPoolExecutor 
+from urllib.parse import urlencode 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 API_KEY = "add13cef-2341-4dbc-9768-7c44da086270"
 
+@dataclass
+class ProductData:
+    name: str = ""
+    title: str = ""
+    url: str = ""
+    is_ad: bool = False
+    pricing_unit: str = ""
+    price: float = None
+    real_price: float = None
+    rating: float = None
+    
+    def __post_init__(self):
+        self.check_string_fields()
+        
+    def check_string_fields(self):
+        for field in fields(self):
+            #this will check string fields
+            if isinstance(getattr(self, field.name), str):
+                #if empty set default value
+                if getattr(self, field.name) == "":
+                    setattr(self, field.name, f"No {field.name} ")
+                    continue
+                #strip the string
+                value = getattr(self, field.name)
+                setattr(self, field.name, value.strip())
+                
+class DataPipeLine: 
+    
+    def __init__(self, csv_filename: str, storage_queue_limit = 50):
+        self.names_seen =[]
+        self.storage_queue = []
+        self.storage_queue_limit = storage_queue_limit
+        self.csv_filename = csv_filename
+        self.csv_file_open = False
+        
+    def save_to_csv(self):
+        self.csv_file_open = True
+        data_to_save =[]
+        data_to_save.extend(self.storage_queue)
+        self.storage_queue.clear()
+        if not data_to_save:
+            return
 
-def search_product(product_name: str ,page_number =1, retries = 3):
+        keys = [field.name for field in fields(data_to_save[0])]
+        file_exists = os.path.exists(self.csv_filename) and os.path.getsize(self.csv_filename) > 0
+        
+        with open(self.csv_filename, mode= 'a', newline='', encoding= "utf8") as output_file:
+            writer = csv.DictWriter(output_file, fieldnames=keys)
+            if not file_exists:
+                writer.writeheader()
+            for item in data_to_save:
+                writer.writerow(asdict(item))
+                
+        self.csv_file_open = False
+        
+    def is_duplicate(self,input_data):
+        if input_data.name in self.names_seen:
+            logger.warning(f"Duplicate data found: {input_data.name}. Item dropped")
+            return True
+        self.names_seen.append( input_data.name)
+        return False
+    
+    def add_data(self, scraped_data):
+        if self.is_duplicate(scraped_data) == False:
+            self.storage_queue.append(scraped_data)
+            if len(self.storage_queue) >= self.storage_queue_limit and self.csv_file_open == False:
+                self.save_to_csv()
+                
+    def close_pipeline(self):
+        if self.csv_file_open:
+            
+            time.sleep(3)
+            
+        if len(self.storage_queue) > 0:
+            self.save_to_csv()
+            
+def get_scrapeops_url(url, location="us"):
+    payload = {
+        "api_key": API_KEY,
+        "url": url, 
+        "country": location
+    }
+    proxy_url = "https://proxy.scrapeops.io/v1/?" + urlencode(payload)
+    return proxy_url
+    
+            
+
+
+def search_products(product_name: str ,page_number =1,location ="us", retries = 3, data_pipeline = None):
     tries = 0 
     success = False
     
     while tries< retries and not success:
         try:
-            url= f"https://www.amazon.com/s?k={product_name}&page={page_number}"
+            url= get_scrapeops_url(
+                f"https://www.amazon.com/s?k={product_name}&page={page_number}",
+                location=location,
+            )
             resp = requests.get(url)
             
             if resp.status_code == 200:
                 logger.info("Successfully fetched page")
                 
                 soup = BeautifulSoup(resp.text, 'html.parser')
+                
+                bad_divs = soup.find_all("div", class_="AdHolder")
+                for bad_div in bad_divs:
+                    bad_div.decompose()
+                
                 divs = soup.find_all("div")
                 last_title= ""
                 
@@ -53,17 +151,17 @@ def search_product(product_name: str ,page_number =1, retries = 3):
                             real_price =float(prices[1].text.replace(pricing_unit, "").replace(",", "")) if len(prices)>1 else price
                         
                         if symbol_presence and rating_present and price_present:
-                            product = {
-                                "name": asin, 
-                                "title": title,
-                                "url": product_url,
-                                "is_ad": ad_status,
-                                "pricing_unit": pricing_unit,
-                                "price": price,
-                                "real_price": real_price,
-                                "rating": rating
-                            }
-                            print(product)
+                            product = ProductData(
+                                name = asin,
+                                title = title,
+                                url = product_url,
+                                is_ad = ad_status,
+                                pricing_unit = pricing_unit,
+                                price = price,
+                                real_price = real_price,
+                                rating = rating,
+                            )
+                            data_pipeline.add_data(product)
                         
                         last_title = title
                         
@@ -84,10 +182,31 @@ def search_product(product_name: str ,page_number =1, retries = 3):
     
     print(f"exited scraped products for: {product_name}")
     
+def threaded_search(product_name, pages, max_workers = 5, location = "us", retries= 3):
+    search_pipeline = DataPipeLine(csv_filename=f"{product_name}.csv")
+    
+    with ThreadPoolExecutor(max_workers = max_workers) as executor:
+        futures =[
+            executor.submit(
+                search_products, product_name, page, location, retries, search_pipeline
+            ) for page in range(1, pages+1)
+            
+        ]
+        
+        for future in futures:
+            future.result()
+    
+    search_pipeline.close_pipeline()
 if __name__ == "__main__":
     PRODUCTS = ["phone"]
     MAX_RETRIES = 2
+    PAGES = 5
+    MAX_THREADS =  3
+    LOCATION = "us"
+    
     
     for product in PRODUCTS:
-        search_product(product, retries =MAX_RETRIES)
+        threaded_search(
+            product, PAGES, max_workers= MAX_THREADS, location=LOCATION, retries = MAX_RETRIES
+        )
         
